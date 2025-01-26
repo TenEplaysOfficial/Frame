bdf1c96cb21fae3c3dc41efea0ebfdd5e7af9b2e
TenE18 <tchinnu5132@gmail.com>
2025-01-26 23:56:23 +0530

    chore: update npm packages to latest versions

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -1,17 +1,17 @@
-import { useParams } from 'react-router-dom';
-import useFetch from '../hooks/useFetch';
+// import { useParams } from 'react-router-dom';
+// import useFetch from '../hooks/useFetch';
 
 export default function Explore() {
-  const { id, media_type } = useParams();
-  //   console.log('Profile ID:', id);
+  // const { id, media_type } = useParams();
+
+  // console.log('Profile ID:', id);
   // console.log('Profile media_type:', media_type);
+  // console.log('Profile Data:', data);
 
-  const { data, isLoading, errorMessage } = useFetch({
-    urlType: 'findByID',
-    findByID: id,
-    category: media_type,
-  });
-  console.log('Profile Data:', data);
-
+  // const { data, isLoading, errorMessage } = useFetch({
+  //   urlType: 'findByID',
+  //   findByID: id,
+  //   category: media_type,
+  // });
   return <div className="pt-20"></div>;
 }
